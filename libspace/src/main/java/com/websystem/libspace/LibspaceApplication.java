73828fe7f3f4a37333fec2677babc5d665c5a743
pereiraR3 <anthony_rodriguespereira@outlook.com>
2024-06-26 18:45:20 -0400

    Init Project

---
+++ libspace/src/main/java/com/websystem/libspace/LibspaceApplication.java
@@ -0,0 +1,13 @@
+package com.websystem.libspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibspaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibspaceApplication.class, args);
+	}
+
+}
---
+++ libspace/src/test/java/com/websystem/libspace/LibspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websystem.libspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibspaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
