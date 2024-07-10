fa13322951290aa5c7bcfe2cc246020052ceba51
pereira3r <anthony_rodriguespereira@outlook.com>
2024-07-10 10:40:41 -0400

    Add & Configure LivroEbookResponseDTO.java with data:
        // -> Relativo a classe pai (Livro)
    
            Long id,
    
            // -> Relativo a classe filha (LivroEbook)
    
            Short tamanho_arquivo,
    
            String formato_arquivo

--- libspace/src/main/java/com/websystem/libspace/domain/livro_ebook/LivroEbookResponseDTO.java
+++ libspace/src/main/java/com/websystem/libspace/domain/livro_ebook/LivroEbookResponseDTO.java
@@ -1,4 +1,26 @@
 package com.websystem.libspace.domain.livro_ebook;
 
-public record LivroEbookResponseDTO() {
+public record LivroEbookResponseDTO(
+
+        // -> Relativo a classe pai (Livro)
+
+        Long id,
+
+        // -> Relativo a classe filha (LivroEbook)
+
+        Short tamanho_arquivo,
+
+        String formato_arquivo
+
+) {
+
+    public LivroEbookResponseDTO(LivroEbook livroEbook){
+        this(
+
+                livroEbook.getLivro().getId(),
+                livroEbook.getTamanho_arquivo(),
+                livroEbook.getFormato_arquivo()
+        );
+    }
+
 }
