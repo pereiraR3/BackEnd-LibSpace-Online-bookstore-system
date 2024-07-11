998a71ecbce3f24d007778332e468520d82370eb
pereira3r <anthony_rodriguespereira@outlook.com>
2024-07-11 13:23:00 -0400

    Create LivroEbookUpdateDTO.java

--- libspace/src/main/java/com/websystem/libspace/domain/livro_ebook/LivroEbookUpdateDTO.java
+++ libspace/src/main/java/com/websystem/libspace/domain/livro_ebook/LivroEbookUpdateDTO.java
@@ -1,4 +1,19 @@
 package com.websystem.libspace.domain.livro_ebook;
 
-public record LivroEbookUpdateDTO() {
+import jakarta.validation.constraints.NotNull;
+
+public record LivroEbookUpdateDTO(
+
+        // - Relativo a classe pai (Livro)
+
+        @NotNull
+        Long id_livro,
+
+        // - Relativo a classe filha (LivroEbook)
+
+        Short tamanho_arquivo,
+
+        String formato_arquivo
+
+) {
 }
