a4826b7405ae2e31839c31534d8f10c98d822fa5
pereira3r <anthony_rodriguespereira@outlook.com>
2024-07-11 13:17:20 -0400

    Create LivroAudiobookRepository

---
+++ libspace/src/main/java/com/websystem/libspace/repository/LivroAudiobookRepository.java
@@ -0,0 +1,10 @@
+package com.websystem.libspace.repository;
+
+import com.websystem.libspace.domain.livro_audiobook.LivroAudiobook;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LivroAudiobookRepository extends JpaRepository<LivroAudiobook, Long> {
+
+
+
+}
