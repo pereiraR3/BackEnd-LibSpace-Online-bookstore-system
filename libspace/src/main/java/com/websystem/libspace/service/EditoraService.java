85574b14be2a2230fda8db2ecb0f923669a647c4
pereira3r <anthony_rodriguespereira@outlook.com>
2024-07-11 13:21:33 -0400

    Remove desnecessary imports

--- libspace/src/main/java/com/websystem/libspace/service/EditoraService.java
+++ libspace/src/main/java/com/websystem/libspace/service/EditoraService.java
@@ -1,17 +1,13 @@
 package com.websystem.libspace.service;
 
 import com.websystem.libspace.domain.editora.*;
-import com.websystem.libspace.domain.livro.LivroListeningDTO;
-import com.websystem.libspace.domain.livro.LivroResponseDTO;
 import com.websystem.libspace.repository.EditoraRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
