65773d8ab5ce0128e0c872c484bc3626f870f6fc
pereira3r <anthony_rodriguespereira@outlook.com>
2024-07-11 13:17:39 -0400

    Create LivroAudiobookRequestDTO

---
+++ libspace/src/main/java/com/websystem/libspace/domain/livro_audiobook/LivroAudiobookRequestDTO.java
@@ -0,0 +1,31 @@
+package com.websystem.libspace.domain.livro_audiobook;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record LivroAudiobookRequestDTO(
+
+
+        // - Relativo a classe pai (Livro)
+
+        @NotNull
+        Long id,
+
+        // - Relativo a classe filho (LivroAudiobook)
+
+        @NotNull
+        Short tamanho_arquivo,
+
+        @NotNull
+        Short formato_arquivo,
+
+        @NotBlank
+        @Size(max = 120)
+        String narrador,
+
+        @NotBlank
+        String url_download
+
+) {
+}
