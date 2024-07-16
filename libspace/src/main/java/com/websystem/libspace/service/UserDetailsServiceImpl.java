8a06abf00110316bad28a148c6753800c1f8cfab
pereira3r <anthony_rodriguespereira@outlook.com>
2024-07-15 20:44:20 -0400

    Create UserDetailsServiceImpl.java

---
+++ libspace/src/main/java/com/websystem/libspace/service/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.websystem.libspace.service;
+
+import com.websystem.libspace.domain.users.UserAuthenticated;
+import com.websystem.libspace.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserAuthenticated loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .map(UserAuthenticated::new)
+                .orElseThrow(() -> new UsernameNotFoundException("Nome de usuário não encontrado"));
+    }
+}
