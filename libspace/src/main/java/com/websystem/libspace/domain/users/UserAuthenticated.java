537ecdb44405846e26ecbd26bbf270649c511ebe
pereira3r <anthony_rodriguespereira@outlook.com>
2024-07-15 20:43:51 -0400

    Create UserAuthenticated.java

---
+++ libspace/src/main/java/com/websystem/libspace/domain/users/UserAuthenticated.java
@@ -0,0 +1,55 @@
+package com.websystem.libspace.domain.users;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class UserAuthenticated implements UserDetails {
+
+    private final User user;
+
+    public UserAuthenticated(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if(this.user.getRole() == UserRole.CLIENTE)
+            return List.of(new SimpleGrantedAuthority("CLIENTE"));
+        else
+            return List.of(new SimpleGrantedAuthority("CLIENTE"));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
