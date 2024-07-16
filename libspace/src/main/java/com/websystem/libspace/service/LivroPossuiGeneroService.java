89f5eb33f7202fc1cc88da6f45ba5aad7a94e65c
pereira3r <anthony_rodriguespereira@outlook.com>
2024-07-16 10:57:23 -0400

    Create LivroPossuiGeneroService.java

---
+++ libspace/src/main/java/com/websystem/libspace/service/LivroPossuiGeneroService.java
@@ -0,0 +1,84 @@
+package com.websystem.libspace.service;
+
+import com.websystem.libspace.domain.genero.Genero;
+import com.websystem.libspace.domain.livro.Livro;
+import com.websystem.libspace.domain.livro_possui_genero.LivroPossuiGenero;
+import com.websystem.libspace.domain.livro_possui_genero.LivroPossuiGeneroResponseDTO;
+import com.websystem.libspace.repository.LivroPossuiGeneroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LivroPossuiGeneroService {
+
+    @Autowired
+    private LivroPossuiGeneroRepository livroPossuiGeneroRepository;
+
+    @Autowired
+    private LivroService livroService;
+
+    @Autowired
+    private GeneroService generoService;
+
+    public LivroPossuiGeneroResponseDTO create(Long idLivro, Long idCategoria){
+
+        Livro livro = livroService.findById(idLivro);
+
+        Genero genero = generoService.findById(idCategoria);
+
+        LivroPossuiGenero livroPossuiGenero = new LivroPossuiGenero(livro, genero);
+
+        livroPossuiGeneroRepository.save(livroPossuiGenero);
+
+        return new LivroPossuiGeneroResponseDTO(livroPossuiGenero);
+
+    }
+
+    public List<LivroPossuiGeneroResponseDTO> findByLivroId(Long idLivro){
+
+
+        generoService.findById(idLivro);
+
+        return livroPossuiGeneroRepository.findAllByLivroId(idLivro).stream().map(LivroPossuiGeneroResponseDTO::new).toList();
+
+    }
+
+    public List<LivroPossuiGeneroResponseDTO> findByCategoriaId(Long idCategoria){
+
+        generoService.findById(idCategoria);
+
+        return livroPossuiGeneroRepository.findAllByGeneroId(idCategoria).stream().map(LivroPossuiGeneroResponseDTO::new).toList();
+
+    }
+
+    private LivroPossuiGenero findByLivroIdCategoriaId(Long idLivro, Long idCategoria) {
+
+
+        Livro livro = livroService.findById(idLivro);
+
+        Genero genero = generoService.findById(idCategoria);
+
+        return livroPossuiGeneroRepository.findByLivroIdAndGeneroId(livro.getId(), genero.getId());
+
+    }
+
+    public List<LivroPossuiGeneroResponseDTO> findAll(){
+
+        return livroPossuiGeneroRepository.findAll().stream().map(LivroPossuiGeneroResponseDTO::new).collect(Collectors.toList());
+
+    }
+
+    public void deleteByLivroIdByGeneroId(Long idLivro, Long idCategoria){
+
+        Livro livro = livroService.findById(idLivro);
+
+        Genero genero = generoService.findById(idCategoria);
+
+        livroPossuiGeneroRepository.deleteByLivroIdAndGeneroId(idLivro, idCategoria);
+
+    }
+
+
+
+}
