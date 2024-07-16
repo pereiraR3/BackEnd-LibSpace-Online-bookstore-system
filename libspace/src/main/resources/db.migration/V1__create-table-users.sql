9d7cf5dfc9665be63d53d25a51870177af79d12f
pereira3r <anthony_rodriguespereira@outlook.com>
2024-07-15 20:45:06 -0400

    Change V1

--- libspace/src/main/resources/db.migration/V1__create-table-users.sql
+++ libspace/src/main/resources/db.migration/V1__create-table-users.sql
@@ -2,11 +2,11 @@
 CREATE TABLE users (
     id BIGSERIAL PRIMARY KEY,
     cpf CHAR(11) UNIQUE NOT NULL,
-    nome VARCHAR(60) NOT NULL, 
-    senha TEXT NOT NULL, 
+    username VARCHAR(60) UNIQUE NOT NULL,
+    password VARCHAR(80) NOT NULL,
     email VARCHAR(120) UNIQUE NOT NULL,
-    url_foto TEXT,
-    url_website TEXT, 
+    url_foto VARCHAR(1000),
+    url_website VARCHAR(1000),
     bio VARCHAR(2000) NOT NULL,
     role VARCHAR(20) NOT NULL
 );
