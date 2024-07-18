79fb467bdd565c729189597bc2b0665eeae64f00
pereira3r <anthony_rodriguespereira@outlook.com>
2024-07-18 19:13:29 -0400

    Create AvaliacaoRepository.java

---
+++ libspace/src/main/java/com/websystem/libspace/repository/AvaliacaoRepository.java
@@ -0,0 +1,10 @@
+package com.websystem.libspace.repository;
+
+import com.websystem.libspace.domain.avaliacao.Avaliacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AvaliacaoRepository extends JpaRepository<Avaliacao, Long> {
+
+
+
+}
