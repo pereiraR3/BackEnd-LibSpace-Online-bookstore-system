b5dd7ca5fceea627f3486d9a5d2868728c04a220
pereira3r <anthony_rodriguespereira@outlook.com>
2024-07-20 13:40:53 -0400

    Create Oferta.java

---
+++ libspace/src/main/java/com/websystem/libspace/domain/oferta/Oferta.java
@@ -0,0 +1,44 @@
+package com.websystem.libspace.domain.oferta;
+
+import com.websystem.libspace.domain.editora.Editora;
+import com.websystem.libspace.domain.livro.Livro;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "oferta")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Oferta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "id_livro", referencedColumnName = "id")
+    private Livro livro;
+
+    @OneToOne
+    @JoinColumn(name = "id_editora", referencedColumnName = "id")
+    private Editora editora;
+
+    @Column(name  = "preco", nullable = false)
+    private double preco;
+
+    @Column(name = "desconto", nullable = true)
+    private double desconto;
+
+
+    public Oferta(Livro livro, Editora editora){
+        this.editora = editora;
+        this.livro = livro;
+        this.preco = livro.getPreco();
+    }
+
+}
