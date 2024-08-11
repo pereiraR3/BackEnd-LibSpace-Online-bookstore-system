beb713f2fe28c3e4b5ed20ba5ea6e4d55a9c4c4b
pereira3r <anthony_rodriguespereira@outlook.com>
2024-08-11 13:08:31 -0400

    add changes

--- libspace/src/main/java/com/websystem/libspace/controller/LivroController.java
+++ libspace/src/main/java/com/websystem/libspace/controller/LivroController.java
@@ -9,13 +9,10 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/livro")
